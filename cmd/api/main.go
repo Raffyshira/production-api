4c0ac31effc79546c22983c0ceed21e7d31a2908
raffyshira <rafiahsiraprayoga@gmail.com>
2026-08-18 14:34:04 +0700

    feat: skaffold and database module

---
+++ cmd/api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/raffyshira/project-rest-api/internal/db"
+	"github.com/raffyshira/project-rest-api/internal/env"
+	"github.com/raffyshira/project-rest-api/internal/store"
+)
+
+const version = "0.0.1"
+
+func main() {
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://myuser:Rafi330646859@localhost:5432/socialnetwork?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
+		env: env.GetString("ENV", "development"),
+	}
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+	log.Println("Database connection pool established")
+
+	store := store.NewStorage(db)
+
+	app := &application{
+		config: cfg,
+		store:  store,
+	}
+
+	r := app.mount()
+
+	log.Fatal(app.run(r))
+}
---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/raffyshira/project-rest-api
+
+go 1.26.5
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
+	github.com/go-chi/chi/v5 v5.3.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.30.3 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.28.0 // indirect
+	golang.org/x/crypto v0.55.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/text v0.41.0 // indirect
+)
