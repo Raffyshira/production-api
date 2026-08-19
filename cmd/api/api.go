b2d8f9968598466278821619e4e6782266950e0f
raffyshira <rafiahsiraprayoga@gmail.com>
2026-08-19 12:17:59 +0700

    feat: user feed & pagination and filtering

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -55,6 +55,20 @@ func (app *application) mount() http.Handler {
 				r.Patch("/", app.updatePostHandler)
 			})
 		})
+
+		r.Route("/users", func(r chi.Router) {
+			r.Route("/{userID}", func(r chi.Router) {
+				r.Use(app.userContextMiddleware)
+
+				r.Get("/", app.getUserHandler)
+				r.Put("/", app.followUserHandler)
+				r.Put("/", app.unfollowUserHandler)
+			})
+
+			r.Group(func(r chi.Router) {
+				r.Get("/feed", app.getUserFeedHandler)
+			})
+		})
 	})
 
 	return r
