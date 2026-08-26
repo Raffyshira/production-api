784a5b7dfd291caa34a1947890f6e801fd10ceeb
raffyshira <rafiahsiraprayoga@gmail.com>
2026-08-26 19:12:40 +0700

    feat: metrics & fix some cache code

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"expvar"
 	"fmt"
 	"net/http"
 	"os"
@@ -15,6 +16,7 @@ import (
 	"github.com/go-chi/cors"
 	"github.com/raffyshira/project-rest-api/docs"
 	"github.com/raffyshira/project-rest-api/internal/auth"
+	"github.com/raffyshira/project-rest-api/internal/env"
 	"github.com/raffyshira/project-rest-api/internal/mailer"
 	"github.com/raffyshira/project-rest-api/internal/ratelimiter"
 	"github.com/raffyshira/project-rest-api/internal/service"
@@ -89,19 +91,18 @@ type dbConfig struct {
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
 
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedOrigins:   []string{env.GetString("CORS_ALLOWED_ORIGIN", "http://localhost:5174")},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
 	r.Use(app.RateLimiterMiddleware)
 
 	// Set a timeout value on the request context (ctx), that will signal
@@ -111,6 +112,7 @@ func (app *application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+		r.With(app.BasicAuthMiddleware()).Get("/debug/vars", expvar.Handler().ServeHTTP)
 
 		docsURL := fmt.Sprintf("%s/swagger/doc.json", app.config.addr)
 		r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL(docsURL)))
--- internal/store/cache/mocks.go
+++ internal/store/cache/mocks.go
@@ -26,3 +26,7 @@ func (s *MockUserStore) Set(ctx context.Context, user *store.User) error {
 	args := s.Called(user)
 	return args.Error(0)
 }
+
+func (m *MockUserStore) Delete(ctx context.Context, userID int64) {
+	m.Called(userID)
+}
